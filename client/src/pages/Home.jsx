02af2c5c48854a9484df014e56ea1a9a16c61b2b
ojongfranck <ojongfranckronald@gmail.cocm>
2023-11-01 04:20:08 -0700

    working on  the home and almost done with the terms and privacy page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import { SEO } from '../components'
 import { Link } from 'react-router-dom';
+import './Home.css'
 export default function Home() {
     return (
-        <div >
+        <div className="all">
+        <div className='nav'>
             <SEO
                 title='Teach Review'
                 description='A web application to review on lecturers'
                 name='Home'
                 type='Welome'
             />
-            <Link to='/login' className="btn">Login</Link>
-            <Link to='/signup' className="btn">Signup</Link>
+            <p className='text-3xl font-bold'>Teach Review</p>
+            <div className="right">
 
-            <Link to='/#contact' className="btn">Contact</Link>
-            <Link to='/review' className="btn">Review</Link>
-            <Link to='/about' className="btn">about</Link>
+            <Link to='/Home' className="text-2xl font-semibold">Home</Link>
+            <Link to='/login' className="text-2xl font-semibold">Login</Link>
+            <Link to='/signup' className="text-2xl font-semibold">Signup</Link>
+
+            {/* <Link to='/#contact' className="text-center text-3xl font-semibold mb-12">Contact</Link> */}
+            <Link to='/review' className="text-2xl font-semibold">Review</Link>
+            <Link to='/about' className="text-2xl font-semibold">About</Link>
             {/* <Link to='/#contact' className="btn">contact</Link> */}
+          
+            </div>
+            
+        </div>
+
+        <div className="hero">
+            <p>ojong</p>
+        </div>
+
+        <footer>
+            <p>franck</p>
+        </footer>
         </div>
     )
 }
